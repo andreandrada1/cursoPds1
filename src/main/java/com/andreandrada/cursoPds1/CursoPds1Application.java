dda969440f9d8080617d1c2e3012e3cca7f181b8
andreandrada1 <andreandrada1@hotmail.com>
2017-09-14 20:29:26 -0300

    Projeto criato

---
+++ src/main/java/com/andreandrada/cursoPds1/CursoPds1Application.java
@@ -0,0 +1,12 @@
+package com.andreandrada.cursoPds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoPds1Application.class, args);
+	}
+}
